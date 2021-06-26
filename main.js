e6dea4905540e83538d97cccc01ab8c7b60274f1
m4rcelodiasBR <m4rcelodias@live.com>
2021-06-26 18:04:04 -0300

    Terminando o projeto. Aula 5

--- main.js
+++ main.js
@@ -19,10 +19,10 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
-  
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -37,7 +37,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* Scrollreveal: Mostrar elementos quando der scroll na página */
@@ -49,7 +55,7 @@ const scrollReveal = ScrollReveal({
 })
 
 scrollReveal.reveal(
-`#home .text, #home .image,
+  `#home .text, #home .image,
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
@@ -60,9 +66,9 @@ scrollReveal.reveal(
 )
 
 /* Botão voltar para o topo */
-function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector('.back-to-top')
 
+function backToTop() {
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -70,8 +76,34 @@ function backToTop() {
   }
 }
 
+/* menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 /*When scroll*/
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
